8bb9c5a6d9aef40ceb243fb69d07b66aafc0259c
VIctor <colen.victor@icloud.com>
2022-08-19 08:45:37 -0300

    Initial Commit

---
+++ ioasys Empresas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ioasys Empresas
+//
+//  Created by Victor Colen on 19/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
